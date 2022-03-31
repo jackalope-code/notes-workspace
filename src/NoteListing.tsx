8e9b7ed05d6c07d98a6ac9e356344adb5c1f028d
Sean <seanphelan97@gmail.com>
2022-03-30 18:14:37 -0600

    some component cleanup

--- src/NoteListing.tsx
+++ src/NoteListing.tsx
@@ -1,22 +1,13 @@
 import { DataStore } from "aws-amplify";
-import { convertFromRaw, Editor, EditorState } from "draft-js";
 import update from 'immutability-helper'
 import { useCallback, useEffect, useState } from "react";
-import Draggable from "react-draggable";
 import { Note } from "./models";
 import NoteCard from "./NoteCard";
-import ReminderCardQuickList from "./ReminderCardQuickList";
-import { Dialog } from "@mui/material";
-import { PredicateAll } from "@aws-amplify/datastore/lib-esm/predicates";
 
 // TODO: fix prop drilling
 interface NoteListingProps {
     // initialValues: Note[];
     onDelete: (id: string) => void;
-}
-
-interface SwappableCard extends Note {
-    index: number
 }
 
 const NoteListing = ({onDelete}: NoteListingProps) => {
@@ -28,14 +19,9 @@ const NoteListing = ({onDelete}: NoteListingProps) => {
   // TODO: refactor this and make it an observablequery
   // sort by new notes. add to the top. add filtering controls.
   async function fetchNotes() {
-    // const res: any = await graphqlCall(listNotes, null, currentSession);
-    // setNoteItems(res.data?.listNotes.items);
     async function queryNotes() {
-        console.log('attempting to query notes');
         const notes = await DataStore.query(Note);
-        if(notes !== null && notes !== undefined ) {
-          setNotes(notes);
-        }
+        setNotes(notes);
     }
 
     try {
@@ -72,59 +58,52 @@ const NoteListing = ({onDelete}: NoteListingProps) => {
     fetchNotes();
   }
 
-    const moveCard = useCallback((dragIndex: number, hoverIndex: number) => {
-        setNotes((prevNotes: Note[]) =>
-          update(prevNotes, {
-            $splice: [
-              [dragIndex, 1],
-              [hoverIndex, 0, prevNotes[dragIndex] as Note],
-            ],
-          }),
-        )
-      }, [])
-  
-      const renderCard = useCallback(
-        (card: Note, index: number) => {
-          DataStore.save(
-            Note.copyOf(card, updated => {
-              updated.order = index;
-            })
-          );
-          return (
-            <NoteCard
-                  key={card.id}
-                  noteId={card.id}
-                  index={index}
-                  title={card.title}
-                  rawContent={card.content}
-                  handleDeleteNote={onDelete}
-                  moveCard={moveCard}
-            />
-            // <Card
-            //   key={card.id}
-            //   index={index}
-            //   id={card.id}
-            //   text={card.text}
-            //   moveCard={moveCard}
-            // />
-          )
-        },
-        [],
+  const moveCard = useCallback((dragIndex: number, hoverIndex: number) => {
+      setNotes((prevNotes: Note[]) =>
+        update(prevNotes, {
+          $splice: [
+            [dragIndex, 1],
+            [hoverIndex, 0, prevNotes[dragIndex] as Note],
+          ],
+        }),
       )
-    
-    // TODO: is this ok in render?
-    let output;
-    if(notes.length === 0) {
-        output = <div>No notes</div>;
-    } else {
-        output = (
-        <div style={{display: "flex", flexDirection: "column", height: "100vh", padding: "10px"}}>
-            <button onClick={refreshNotesList}>List notes</button>
-            {notes.map((card, i) => renderCard(card, i))}
-        </div>);
-    }
+    }, [])
 
-    return output;
+    const renderCard = useCallback(
+      (card: Note, index: number) => {
+        DataStore.save(
+          Note.copyOf(card, updated => {
+            updated.order = index;
+          })
+        );
+        return (
+          <NoteCard
+                key={card.id}
+                noteId={card.id}
+                index={index}
+                title={card.title}
+                rawContent={card.content}
+                handleDeleteNote={onDelete}
+                moveCard={moveCard}
+          />
+        )
+      },
+      [],
+    )
+  
+  // TODO: is this ok in render?
+  let output;
+  if(notes.length === 0) {
+    output = <div>No notes</div>;
+  } else {
+    output = (
+    <div style={{display: "flex", flexDirection: "column", height: "100vh", padding: "10px"}}>
+        <button onClick={refreshNotesList}>List notes</button>
+        {notes.map((card, i) => renderCard(card, i))}
+    </div>);
+  }
+
+  return output;
 }
 
 export default NoteListing;
